4a8d417b4f66af690d1ecddeeff79d88d0748975
Aaron Xu <aaronxu@MacBook.local>
2019-09-07 23:20:34 +0800

    first commit

--- dogcat/test.sh
+++
@@ -1,6 +0,0 @@
-# test - r for noise amount, s for random seed
-CUDA_VISIBLE_DEVICES=0 python3 CE.py --r=0.1 --s=1234
-CUDA_VISIBLE_DEVICES=0 python3 FW.py --r=0.1 --s=1234
-CUDA_VISIBLE_DEVICES=0 python3 GCE.py --r=0.1 --s=1234
-CUDA_VISIBLE_DEVICES=0 python3 LCCN.py --r=0.1 --s=1234
-CUDA_VISIBLE_DEVICES=0 python3 DMI.py --r=0.1 --s=1234
--- fashion/test.sh
+++
@@ -1,4 +0,0 @@
-# test - r for noise amount, s for random seed, c for case
-CUDA_VISIBLE_DEVICES=1 python3 main.py --r=0.1 --s=1234 --c=1
-CUDA_VISIBLE_DEVICES=1 python3 main.py --r=0.1 --s=1234 --c=2
-CUDA_VISIBLE_DEVICES=1 python3 main.py --r=0.1 --s=1234 --c=3
